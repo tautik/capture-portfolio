bd2d9bd063f43c956feb800956c631f3fe66e6b2
Tautik  Agrahari <tautikagrahari@gmail.com>
2022-07-23 10:51:14 +0530

    4.Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>Any Questoins</h2>
+      <div className="questions">
+        <h4>How do I start?</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          consectetur?
+        </p>
+      </div>
+      <div className="questions">
+        <h4>Daily Schedule</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          consectetur?
+        </p>
+      </div>
+      <div className="questions">
+        <h4>Different Payment Methods</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          consectetur?
+        </p>
+      </div>
+      <div className="questions">
+        <h4>What do i offer</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          consectetur?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,14 @@ import React from "react";
 //importing components
 import AboutSection from "../components/AboutSection";
 import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const About = () => {
   return (
     <div>
       <AboutSection />
       <ServiceSection />
+      <FaqSection />
     </div>
   );
 };
